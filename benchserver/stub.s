f63575a64c7895f98b4f2819f6e01f66bf179c9f
Minux <minux@golang.org>
2015-05-27 20:09:54 -0400

    all: add copyright header.
    Signed-off-by: Minux <minux@golang.org>

--- benchserver/stub.s
+++ benchserver/stub.s
@@ -1,2 +1,6 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // this file is to make sure that go tool doesn't think this is
 // a pure Go package (no -complete argument to the gc compiler)
